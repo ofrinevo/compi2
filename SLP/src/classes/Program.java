bed6191421b0f5963a4b84209723273b30369b5e
elior <xelior.la@gmail.com>
2015-12-11 15:47:53 +0200

    dfdsf

--- SLP/src/classes/Program.java
+++ SLP/src/classes/Program.java
@@ -27,8 +27,41 @@ public class Program extends ASTNode {
 		//Methods are of the class LibraryMethod
 		listMethods.add(new LibraryMethod(new PrimitiveType(0,DataTypes.INT),"random",listForm));
 		
+		////////// PRINTLN
+		form=new Formal(new PrimitiveType(0,DataTypes.STRING),"s");
+		listForm=new ArrayList<Formal>();
+		listForm.add(form);
+		//Methods are of the class LibraryMethod
+		listMethods.add(new LibraryMethod(new PrimitiveType(0,DataTypes.VOID),"println",listForm));
+		//////////////PRINT
+		form=new Formal(new PrimitiveType(0,DataTypes.STRING),"s");
+		listForm=new ArrayList<Formal>();
+		listForm.add(form);
+		//Methods are of the class LibraryMethod
+		listMethods.add(new LibraryMethod(new PrimitiveType(0,DataTypes.VOID),"print",listForm));
+		////////////////////PRINTI (INT)
+		form=new Formal(new PrimitiveType(0,DataTypes.INT),"i");
+		listForm=new ArrayList<Formal>();
+		listForm.add(form);
+		//Methods are of the class LibraryMethod
+		listMethods.add(new LibraryMethod(new PrimitiveType(0,DataTypes.VOID),"printi",listForm));
+		/////////////////////PRINTB(BOOLEAN)
+		form=new Formal(new PrimitiveType(0,DataTypes.BOOLEAN),"b");
+		listForm=new ArrayList<Formal>();
+		listForm.add(form);
+		//Methods are of the class LibraryMethod
+		listMethods.add(new LibraryMethod(new PrimitiveType(0,DataTypes.VOID),"printb",listForm));
+		//////////////////////READI
+		form=new Formal(new PrimitiveType(0,DataTypes.VOID),"");
+		listForm=new ArrayList<Formal>();
+		listForm.add(form);
+		//Methods are of the class LibraryMethod
+		listMethods.add(new LibraryMethod(new PrimitiveType(0,DataTypes.INT),"readi",listForm));
+		
+		
 		ICClass libClass=new ICClass(0,"Library",new ArrayList<Field>(),listMethods);
 		classes.add(0, libClass);
+		
 	}
 
 	public List<ICClass> getClasses() {
