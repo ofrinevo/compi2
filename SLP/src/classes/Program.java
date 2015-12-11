4c6aa2f478acba0cc0800221b9bbaf7d53ac7410
ofrinevo <ofrine@gmail.com>
2015-12-11 16:08:20 +0200

    asfqewfr

--- SLP/src/classes/Program.java
+++ SLP/src/classes/Program.java
@@ -49,10 +49,7 @@ public class Program extends ASTNode {
 		listForm.add(form);
 		listMethods.add(new LibraryMethod(new PrimitiveType(0,DataTypes.VOID),"printb",listForm));
 		//////////////////////READI
-<<<<<<< HEAD
-=======
-		form=new Formal(new PrimitiveType(0,DataTypes.VOID ),"");
->>>>>>> branch 'master' of https://github.com/ofrinevo/compi2.git
+
 		listForm=new ArrayList<Formal>();
 		listMethods.add(new LibraryMethod(new PrimitiveType(0,DataTypes.INT),"readi",listForm));
 		
